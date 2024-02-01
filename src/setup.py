540b3060d4d8903ab82f374504f403f2da554fd9
Dante-Berth <bertinalexAI@protonmail.com>
2024-02-01 23:09:49 +0100

    [UPDATE] minor

--- src/setup.py
+++ src/setup.py
@@ -1,15 +1,16 @@
-import pytorch_lightning
-import torch
 import pytorch_lightning as pl
+import torch
+from torch.utils.data import DataLoader
 from model import cs_ann
 from torch_sequencer import AtomicSequencer
-from torch.utils.data import DataLoader
-class LitModelCs(pl.LightningModule):
-    def __init__(self, model):
-        # https://lightning.ai/docs/pytorch/stable/common/lightning_module.html
-        super().__init__()
-        self.model = model
 
+class LitModelCs(pl.LightningModule):
+    def __init__(self, model, PATH=r"./data/binance-BTCUSDT-5m.pkl",batch_size=128,learning_rate=1e-4):
+        super().__init__()
+        self.save_hyperparameters("learning_rate")
+        self.model = model
+        self.batch_size = batch_size
+        self.PATH = PATH
     def training_step(self, batch, batch_idx):
         x, y = batch
         z = self.model(x)
@@ -24,10 +25,14 @@ class LitModelCs(pl.LightningModule):
         self.log("validation_loss", loss, on_step=True, on_epoch=True, prog_bar=True)
         return loss
 
-
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-4)
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
         return optimizer
+
+    def train_dataloader(self):
+        train_atomic_dataset = AtomicSequencer(PATH=PATH, begin_date="2018-05-01", end_date="2021-11-03")
+        train_dataloader = DataLoader(train_atomic_dataset, batch_size=self.batch_size, shuffle=True, num_workers=6)
+        return train_dataloader
 
 if __name__=="__main__":
     PATH = r"./data/binance-BTCUSDT-5m.pkl"
@@ -49,14 +54,15 @@ if __name__=="__main__":
             "max_pool_reduction":1
         },
     }
+
     batch_size = 128
+    
+    train_atomic_dataset = AtomicSequencer(PATH=PATH, begin_date="2018-05-01", end_date="2021-11-03")
+    train_dataloader = DataLoader(train_atomic_dataset, batch_size=batch_size, shuffle=True, num_workers=6)
 
 
     cs_model = cs_ann(config,config_problem=None,channels_first=True)
-
-    train_atomic_dataset = AtomicSequencer(PATH=PATH, begin_date="2018-05-01", end_date="2021-11-03")
-    train_dataloader = DataLoader(train_atomic_dataset, batch_size=batch_size, shuffle=True, num_workers=6)
-    
+   
 
     mlp_model = torch.nn.Sequential(
     torch.nn.Flatten(),
@@ -74,13 +80,20 @@ if __name__=="__main__":
         cs_model,
         mlp_model
     )
+    
     x,z = next(iter(train_dataloader))
     y = combined_model(x)
+    from pytorch_lightning.tuner.tuning import Tuner
+    from pytorch_lightning import Trainer
+    from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+    model = LitModelCs(model=combined_model)
+       
     
     val_atomic_dataset = AtomicSequencer(PATH=PATH, begin_date="2021-11-03", end_date="2022-11-04")
     val_dataloader = DataLoader(val_atomic_dataset, batch_size=batch_size, shuffle=True, num_workers=6)
-    model = LitModelCs(model=combined_model)
-    trainer = pytorch_lightning.Trainer(enable_progress_bar=True)
     
+    trainer = Trainer(enable_progress_bar=True,callbacks=[EarlyStopping(monitor="val_loss", mode="min",patience=30)])
+    tuner = Tuner(trainer)
+    model.hparams.lr = tuner.lr_find(model).suggestion()
     trainer.fit(model, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader)
 
